0c9da6ccf36601a5cfe1206f5a5a80ffa778d399
András Novoszáth <anovoszath@diligent.com>
2023-06-21 17:24:28 +0200

    endpoint tests

--- cdk_workshop/pipeline_stack.py
+++ cdk_workshop/pipeline_stack.py
@@ -32,3 +32,26 @@ class WorkshopPipelineStack(Stack):
         # create an instance of the workshop stack as stage and add to the pipeline
         deploy = WorkshopPipelineStage(self, "Deploy")
         deploy_stage = pipeline.add_stage(deploy)
+
+        # Tests
+        deploy_stage.add_post(
+            pipelines.ShellStep(
+            "TestViewerEndpoint", env_from_cfn_outputs={
+                "ENDPOINT_URL": deploy.hc_viewer_url
+            },
+            commands=['curl -Ssf $ENDPOINT_URL']
+            )
+        )
+        deploy_stage.add_post(
+            pipelines.ShellStep(
+            "TestAPIGateWayEndpoint",
+            env_from_cfn_outputs={
+                "ENDPOINT_URL": deploy.hc_endpoint
+            },
+            commands=[
+                "curl -Ssf $ENDPOINT_URL",
+                "curl -Ssf $ENDPOINT_URL/hello",
+                "curl -Ssf $ENDPOINT_URL/hello/world",
+            ]
+            )
+        )

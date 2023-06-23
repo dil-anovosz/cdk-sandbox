0ad2891cdd9f711979b59d30ac9515b6ae87f667
András Novoszáth <anovoszath@diligent.com>
2023-06-23 15:42:22 +0200

    change source to github

--- cdk_workshop/pipeline_stack.py
+++ cdk_workshop/pipeline_stack.py
@@ -1,5 +1,5 @@
 from constructs import Construct
-from aws_cdk import Stack, aws_codecommit as codecommit, pipelines
+from aws_cdk import Stack, pipelines
 from cdk_workshop.pipeline_stage import WorkshopPipelineStage
 
 
@@ -7,20 +7,16 @@ class WorkshopPipelineStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        repo = codecommit.Repository(
-            self, "WorkshopRepo", repository_name="WorkshopRepo"
-        )
-
         pipeline = pipelines.CodePipeline(
             self,
             "Pipeline",
-            # "synth", describes the commands necessary to build the CDK application from source
-            # should always end to a `synth` command
             synth=pipelines.ShellStep(
                 "Synth",
-                input=pipelines.CodePipelineSource.code_commit(
-                    repo, "main"
-                ),  # the repository of the CDK code
+                input=pipelines.CodePipelineSource.connection(
+                    "dil-anovosz/cdk-sandbox",
+                    "main",
+                    connection_arn="arn:aws:codestar-connections:us-east-1:681724587179:connection/e148970f-1012-4d76-9df2-3448b9d03e87",
+                ),
                 commands=[
                     "npm install -g aws-cdk",
                     "pip install -r requirements.txt",
@@ -29,7 +25,6 @@ class WorkshopPipelineStack(Stack):
             ),
         )
 
-        # create an instance of the workshop stack as stage and add to the pipeline
         deploy = WorkshopPipelineStage(self, "Deploy")
         deploy_stage = pipeline.add_stage(deploy)
 

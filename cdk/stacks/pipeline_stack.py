6a6cfd5c26726a97c83574a44ed46edaec673d51
András Novoszáth <anovoszath@diligent.com>
2023-06-27 17:43:53 +0200

    test, test

--- cdk/stacks/pipeline_stack.py
+++ cdk/stacks/pipeline_stack.py
@@ -70,7 +70,7 @@ class CodePipelineStack(Stack):
         )
         notifier.add_target(topic)
 
-        # Tests, test, test
+        # Tests, test, test, test
         deploy_stage.add_post(
             pipelines.ShellStep(
                 "TestViewerEndpoint",
